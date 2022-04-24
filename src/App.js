cf12fac91687c5ccf8a0e573c42ca98465f3fa29
Yoshimasa Fuwa <little.ducks@icloud.com>
2022-04-24 15:41:17 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const App = () => {
-    return <div>template</div>;
+    const [count, setCount] = useState(0);
+    console.log(count);
+
+    const increment = () => setCount((p) => p + 1);
+    const decrement = () => setCount((p) => p - 1);
+    const reset = () => setCount(0);
+    const bai = () => setCount((p) => p * 2);
+    const waru3 = () => setCount((p) => (p % 3 === 0 ? p / 3 : p));
+
+    return (
+        <>
+            count : {count}
+            <br />
+            <button onClick={increment}>+1</button>
+            <button onClick={decrement}>-1</button>
+            <button onClick={reset}>reset</button>
+            <button onClick={bai}>x2</button>
+            <button onClick={waru3}>3</button>
+        </>
+    );
 };
 
 export default App;
