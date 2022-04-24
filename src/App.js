951fe83bf194d3d6179d00d15ee7319078e3766b
FUWA YOSHIMASA <fuwa.yoshimasa@jp.fujitsu.com>
2022-04-25 08:30:33 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = (props) => {
-    const [state, setState] = useState(props);
+  const [state, setState] = useState(props);
 
-    const reset = () => {
-        setState(props);
-    };
+  const reset = () => {
+    setState(props);
+  };
 
-    return (
-        <>
-            <p>
-                {state.name} = {state.price}
-            </p>
-            <button onClick={() => setState((p) => ({ ...p, price: p.price + 1 }))}>+1</button>
-            <button onClick={() => setState((p) => ({ ...p, price: p.price - 1 }))}>-1</button>
-            <button onClick={reset}>reset</button>
-            <input value={state.name} onChange={(e) => setState((p) => ({ ...p, name: e.target.value }))} />
-        </>
-    );
+  useEffect(() => {
+    console.log("useEffect 1");
+  });
+
+  useEffect(() => {
+    console.log("useEffect 2");
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect 3");
+  }, [state.name]);
+
+  return (
+    <>
+      <p>
+        {state.name} = {state.price}
+      </p>
+      <button onClick={() => setState((p) => ({ ...p, price: p.price + 1 }))}>
+        +1
+      </button>
+      <button onClick={() => setState((p) => ({ ...p, price: p.price - 1 }))}>
+        -1
+      </button>
+      <button onClick={reset}>reset</button>
+      <input
+        value={state.name}
+        onChange={(e) => setState((p) => ({ ...p, name: e.target.value }))}
+      />
+    </>
+  );
 };
 
 App.defaultProps = {
-    name: "",
-    price: 1000,
+  name: "",
+  price: 1000,
 };
 
 export default App;
