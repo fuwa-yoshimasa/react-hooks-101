b1e6a045546deffdd207cf51bffd4bab7832990f
FUWA YOSHIMASA <fuwa.yoshimasa@jp.fujitsu.com>
2022-04-27 08:25:09 +0900

    create reducer for events

---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+const events = (state = [], action) => {
+  switch (action.type) {
+    case "CREATE_EVENT":
+      const maxId = state.reduce((a, b) => Math.max(a.id, b.id));
+      const newId = maxId + 1;
+      const event = { id: newId, title: action.title, body: action.body };
+      return [...state.map, event];
+    case "DELETE_EVENT":
+      return state;
+    case "DELETE_ALL_EVENTS":
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default events;
