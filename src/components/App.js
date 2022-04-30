bc6bebbb8d0687da1d2bad5aefeb2b06b988ebc8
Yoshimasa Fuwa <little.ducks@icloud.com>
2022-05-01 08:51:27 +0900

    disypaly delete

--- src/components/App.js
+++ src/components/App.js
@@ -1,72 +1,67 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useReducer, useState } from "react";
 
+import Event from "./Event";
 import reducers from "../reducers";
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducers, []);
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-  console.log(state);
-  const addEvent = (e) => {
-    e.preventDefault();
-    dispatch({
-      type: "CREATE_EVENT",
-      title,
-      body,
-    });
+    const [state, dispatch] = useReducer(reducers, []);
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
 
-    setTitle("");
-    setBody("");
-  };
+    const addEvent = (e) => {
+        e.preventDefault();
+        dispatch({
+            type: "CREATE_EVENT",
+            title,
+            body,
+        });
 
-  return (
-    <div className="container-fluid">
-      <h4>イベント作成フォーム</h4>
-      <form>
-        <div>
-          <label htmlFor="formEventTitle" className="form-label">
-            タイトル
-          </label>
-          <input
-            className="form-control"
-            id="formEventTitle"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+        setTitle("");
+        setBody("");
+    };
+
+    return (
+        <div className="container-fluid">
+            <h4>イベント作成フォーム</h4>
+            <form>
+                <div>
+                    <label htmlFor="formEventTitle" className="form-label">
+                        タイトル
+                    </label>
+                    <input className="form-control" id="formEventTitle" value={title} onChange={(e) => setTitle(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="formEventBody" className="form-label">
+                        ボディ
+                    </label>
+                    <textarea className="form-control" id="formEventBody" value={body} onChange={(e) => setBody(e.target.value)} />
+                </div>
+
+                <button className="btn btn-primary" onClick={addEvent}>
+                    イベント作成
+                </button>
+                <button className="btn btn-danger">全イベント削除</button>
+
+                <h4>イベント一覧</h4>
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>タイトル</th>
+                            <th>ボディ</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {state.map((event, index) => {
+                            return <Event key={index} event={event} dispatch={dispatch} />;
+                        })}
+                    </tbody>
+                </table>
+            </form>
         </div>
-        <div>
-          <label htmlFor="formEventBody" className="form-label">
-            ボディ
-          </label>
-          <textarea
-            className="form-control"
-            id="formEventBody"
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-          />
-        </div>
-
-        <button className="btn btn-primary" onClick={addEvent}>
-          イベント作成
-        </button>
-        <button className="btn btn-danger">全イベント削除</button>
-
-        <h4>イベント一覧</h4>
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>タイトル</th>
-              <th>ボディ</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody></tbody>
-        </table>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,18 @@
 const events = (state = [], action) => {
-  switch (action.type) {
-    case "CREATE_EVENT":
-      const maxId = state.reduce((a, b) => Math.max(a.id, b.id), 0);
-      const newId = maxId + 1;
-      const event = { id: newId, title: action.title, body: action.body };
-      return [...state, event];
-    case "DELETE_EVENT":
-      return state;
-    case "DELETE_ALL_EVENTS":
-      return [];
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case "CREATE_EVENT":
+            const maxId = state.reduce((a, b) => (a.id > b.id ? a : b), { id: 0 });
+            const newId = maxId.id + 1;
+            const event = { id: newId, title: action.title, body: action.body };
+            return [...state, event];
+        case "DELETE_EVENT":
+            console.log(action.id);
+            return state.filter((event) => event.id !== action.id);
+        case "DELETE_ALL_EVENTS":
+            return [];
+        default:
+            return state;
+    }
 };
 
 export default events;
