9ea721517a6784b78943d72061e56c5498bef206
Yoshimasa Fuwa <little.ducks@icloud.com>
2022-05-05 09:56:15 +0900

    create operation log in case of event deletion

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import { DELETE_EVENT } from "../actions";
+import { timeCurrentIso8601 } from "../utils";
+import { DELETE_EVENT, ADD_OPERATION_LOG } from "../actions";
 import AppContext from "../contexts/AppContext";
 
 const Event = ({ event }) => {
@@ -10,6 +10,11 @@ const Event = ({ event }) => {
         const result = window.confirm(`削除？ ID=${event.id}`);
         if (result) {
             dispatch({ type: DELETE_EVENT, id: event.id });
+            dispatch({
+                type: ADD_OPERATION_LOG,
+                description: `イベント削除 ID=${event.id}`,
+                operatedAt: timeCurrentIso8601(),
+            });
         }
     };
     return (
